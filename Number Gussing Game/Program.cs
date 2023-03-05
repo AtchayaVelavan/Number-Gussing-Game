fc9441f3ad0cad656d6da5d88ab8bad94727477d
AtchayaVelavan <AtchayaVelavan>
2023-03-05 18:33:03 +0530

    Project Added

---
+++ Number Gussing Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Gussing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Number Gussing Game");
+            Random r = new Random();
+            int rno = r.Next(1, 100);
+            Console.WriteLine(rno);
+
+            bool win=false;
+            do
+            {
+                Console.Write("Enter ur number:");
+                int number = int.Parse(Console.ReadLine());
+
+                if(rno>number)
+                {
+                    
+                    Console.WriteLine("To low think high" );
+
+                }
+                else if(rno<number)
+                {
+                 
+                    Console.WriteLine("too high think low");
+                }
+                else if(rno==number)
+                {
+                    Console.WriteLine("Win");
+                    win=true;
+                }
+
+            } while (win == false);
+          
+           
+        }
+    }
+}
